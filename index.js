44ca1d21a278e39ddf2374e636b02fd88efe15a4
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-09 18:13:14 -0600

    commit tree c0efcde2f1c0f6e57b4b060e333c87256c83020f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driverArray, names){
+  return driverArray.filter(name => name.toLowerCase() === names.toLowerCase())
+}
+
+function fuzzyMatch(driverArray, startsWithLetter){
+  const matchingDrivers = driverArray.filter(name => name.indexOf(startsWithLetter) === 0)
+  return matchingDrivers
+}
+
+function matchName(driverArray, string){
+  return driverArray.filter(driver => driver.name  === string)
+}
